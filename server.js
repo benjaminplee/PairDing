0457ead9b6e9c413af657612830ba3a284df254b
Benjamin Lee <leebenjp@gmail.com>
2010-12-28 21:25:43 -0600

    setting up simple static file serving

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
-var http = require('http');
+var http = require('http'),
+    url = require('url'),
+	fs = require('fs');
  
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Hello world\n");
+var server = http.createServer(function(req, res) {
+  var path = url.parse(req.url).pathname;
+  switch (path){
+    case '/':    
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write('<html><body><h1>Hola!</h1></body></html>');
+		res.end();
+		break;	
+	default:
+		fs.readFile(__dirname + path, function(err, data){
+			if (err) {
+				res.writeHead(404, {"Content-Type": "text/plain"});
+				res.end();
+				return;
+			}
+			
+			res.writeHead(200, {'Content-Type': 'text/html'})
+			res.write(data, 'utf8');
+			res.end();
+		});
+		break;
+	}
 });
  
 server.listen(80);
