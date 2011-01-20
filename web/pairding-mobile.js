87ac0559938cfb58ef0dabe2bdf4b689181e24c0
Benjamin Lee <leebenjp@gmail.com>
2011-01-20 15:08:57 -0600

    hacked in status traversal logic and mouse clicks

--- web/pairding-mobile.js
+++ web/pairding-mobile.js
@@ -1,23 +1,95 @@
-var danceCard = {
-	pairs: [ [ { id: 1, name: 'Andrew'}, { id: 2, name: 'Bob'} ],
-	 				 [ { id: 3, name: 'Chris'}, { id: 3, name: 'Dave'} ],
-					 [ { id: 5, name: 'Eric'}, { id: 6, name: 'Frank'} ] ],
-	singles: [ { id: 7, name: 'Greg'}, { id: 8, name: 'Harry'} ],
-	out: [ { id: 9, name: 'Ian'}, { id: 10, name: 'Jack'} ]
+var danceCard = undefined
+var firstPeep = undefined;
+
+function fillDanceCard() {
+	danceCard = {
+		pairs: [ [ { id: 1, name: 'Andrew'}, { id: 2, name: 'Bob'} ],
+		 				 [ { id: 3, name: 'Chris'}, { id: 4, name: 'Dave'} ],
+						 [ { id: 5, name: 'Eric'}, { id: 6, name: 'Frank'} ] ],
+		singles: [ { id: 7, name: 'Greg'}, { id: 8, name: 'Harry'} ],
+		out: [ { id: 9, name: 'Ian'}, { id: 10, name: 'Jack'} ]
+	}
+	firstPeep = undefined
+}
+
+function breakOldPair(pair, newGuy) {
+	if(pair[0].id === uno.id) {
+		danceCard.singles.push(pair[0])
+		return null;
+	}
+	
+	else if(pair[1].id === uno.id) {
+		danceCard.singles.push(pair[1])
+	}
+}
+
+function updateDanceCard(uno, dos) {
+	danceCard.singles = $.map(danceCard.singles, function(single) {
+		if(single.id === uno.id || single.id === dos.id) {
+			return null;
+		}
+		
+		return single;
+	})
+	
+	var alreadyAPair = false;
+	
+	danceCard.pairs = $.map(danceCard.pairs, function(pair) {
+		if( (pair[0].id === uno.id && pair[1].id === dos.id) || 
+		    (pair[0].id === dos.id && pair[1].id === uno.id) ) {
+			danceCard.singles.push(uno)
+			danceCard.singles.push(dos)
+			alreadyAPair = true;
+			return null;
+		}
+		
+		if(pair[0].id === uno.id || pair[0].id == dos.id) {
+			danceCard.singles.push(pair[1])
+			return null;
+		}
+
+		if(pair[1].id === uno.id || pair[1].id == dos.id) {
+			danceCard.singles.push(pair[0])
+			return null;
+		}
+		
+		return [ pair ]; // double array to get around .map auto-flatten
+	})
+
+	if(!alreadyAPair && uno.id != dos.id) {
+		danceCard.pairs.push([ uno, dos ]) 
+	}
+	
+	var wasAnOuter = false
+	
+	danceCard.out = $.map(danceCard.out, function(outer) {
+		if(outer.id == uno.id || outer.id == dos.id) {
+			if(uno.id == dos.id) {
+				wasAnOuter = true;
+				danceCard.singles.push(uno)
+			}
+			
+			return null;
+		}
+		else {
+			return outer;
+		}
+	})
+	
+	if(!wasAnOuter && uno.id == dos.id) {
+		danceCard.out.push(uno)
+	}
 }
 
 $(function() {
 	
-	var firstPeep = undefined;
 	$('.peep').live('click', function() {
 		var clickedPeep = $(this);
 		
 		if(firstPeep) {
-			// break existing relationships
+			updateDanceCard(diet(firstPeep), diet(clickedPeep));
 
-			danceCard.pairs = [ [ diet(firstPeep), diet(clickedPeep) ] ]
-
-			displayPeeps(danceCard)
+			displayPeeps()
 
 			firstPeep = undefined;
 		}
@@ -29,12 +101,13 @@ $(function() {
 		return false
 	})
 		
-	$('h1').click(function() {
-		displayPeeps(danceCard)
+	$('#addPeep').click(function() {
+		fillDanceCard()
+		displayPeeps()
 	})
 	
 	function diet(fatElement) {
-		return { id: firstPeep.id, name: fatElement.children().first().html() }
+		return { id: parseInt(fatElement.attr('id')), name: fatElement.children().first().html() }
 	}
 	
 	var peepsContainer = $('#peeps')
@@ -56,10 +129,10 @@ $(function() {
 		peepsContainer.append('<li class="no-story"><a href="#" class="peep" id="' + this.id + '"><h2>' + this.name + '</h2></a></li>')
 	}
 	
-	function displayPeeps(peeps) {
+	function displayPeeps() {
 		clearContainer()
-		$.each(peeps.pairs, insertPair)
-		$.each(peeps.singles, insertSingle)
-		$.each(peeps.out, insertOut)
+		$.each(danceCard.pairs, insertPair)
+		$.each(danceCard.singles, insertSingle)
+		$.each(danceCard.out, insertOut)
 	}
 });
