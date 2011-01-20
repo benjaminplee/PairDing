305aff435c25d4a7fbac872bb935101cbb88e25b
Benjamin Lee <leebenjp@gmail.com>
2011-01-19 20:27:55 -0600

    added basic logic for changing dancecard

--- web/pairding-mobile.js
+++ web/pairding-mobile.js
@@ -1,9 +1,65 @@
+var danceCard = {
+	pairs: [ [ { id: 1, name: 'Andrew'}, { id: 2, name: 'Bob'} ],
+	 				 [ { id: 3, name: 'Chris'}, { id: 3, name: 'Dave'} ],
+					 [ { id: 5, name: 'Eric'}, { id: 6, name: 'Frank'} ] ],
+	singles: [ { id: 7, name: 'Greg'}, { id: 8, name: 'Harry'} ],
+	out: [ { id: 9, name: 'Ian'}, { id: 10, name: 'Jack'} ]
+}
+
 $(function() {
+	
+	var firstPeep = undefined;
 	$('.peep').live('click', function() {
-		$(this).toggleClass('selected')
+		var clickedPeep = $(this);
 		
+		if(firstPeep) {
+			// break existing relationships
+
+			danceCard.pairs = [ [ diet(firstPeep), diet(clickedPeep) ] ]
+
+			displayPeeps(danceCard)
+
+			firstPeep = undefined;
+		}
+		else {
+			clickedPeep.toggleClass('selected')
+			firstPeep = clickedPeep;
+		}
+				
 		return false
 	})
+		
+	$('h1').click(function() {
+		displayPeeps(danceCard)
+	})
 	
-	// $('#peeps').append('<li class=""><a href="#" class="xPair" id="2"><h2>Matt</h2></a></li>')
+	function diet(fatElement) {
+		return { id: firstPeep.id, name: fatElement.children().first().html() }
+	}
+	
+	var peepsContainer = $('#peeps')
+	
+	function clearContainer() {
+		peepsContainer.html('')
+	}
+	
+	function insertPair() {
+		peepsContainer.append('<li class="pair"><a href="#" class="peep left" id="' + this[0].id + '"><h2>' + this[0].name + '</h2></a></li>')
+		peepsContainer.append('<li class="pair"><a href="#" class="peep right" id="' + this[1].id + '"><h2>' + this[1].name + '</h2></a></li>')		
+	}
+	
+	function insertSingle() {
+		peepsContainer.append('<li><a href="#" class="peep" id="' + this.id + '"><h2>' + this.name + '</h2></a></li>')
+	}
+	
+	function insertOut() {
+		peepsContainer.append('<li class="no-story"><a href="#" class="peep" id="' + this.id + '"><h2>' + this.name + '</h2></a></li>')
+	}
+	
+	function displayPeeps(peeps) {
+		clearContainer()
+		$.each(peeps.pairs, insertPair)
+		$.each(peeps.singles, insertSingle)
+		$.each(peeps.out, insertOut)
+	}
 });
